f1b263022b81ecd62aca2be24c5896f4abc3b343
Manish Shahi <manish.shahi880@gmail.com>
2024-06-25 10:50:13 +0545

    minor change

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,20 @@
-import { defineConfig } from "vite";
+// import { defineConfig } from "vite";
+// export default defineConfig({
+//   build: {
+//     rollupOptions: {
+//       input: {
+//         main: "index.html",
+//         game: "startPage.html",
+//         levelEditor: "levelEditor.html"
+//       },
+//     },
+//   },
+// });
+
+
+import { defineConfig } from 'vite';
 export default defineConfig({
+  base: '/chronicles-adventure/',
   build: {
     rollupOptions: {
       input: {
